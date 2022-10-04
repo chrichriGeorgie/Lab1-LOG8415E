dcac9a75601ae7bf41d510216bf0fee7827dc317
jeromeCleris <jerome.cleris@polymtl.ca>
2022-10-04 17:48:44 -0400

    added startup script with aws credentials input required

---
+++ startup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#AWS credentials configuration
+
+echo AWS Access Key ID:
+read aws_access_key_id
+
+echo AWS Secret Access Key:
+read aws_secret_access_key
+
+echo AWS Session Token:
+read aws_session_token
+
+aws configure set aws_access_key_id $aws_access_key_id
+aws configure set aws_secret_access_key $aws_secret_access_key
+aws configure set aws_session_token $aws_session_token
+
+#Terraform deploying AWS Infrastructure
+
+
+#Docker Clients startup
+
