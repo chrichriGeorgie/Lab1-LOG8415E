70bc58b03565ddc149a8cc63af461cd96e84691f
Jimmy Bell <jimmy.bell@polymtl.ca>
2022-10-05 16:15:08 -0400

    Added base code for elb load balancer with comments.

--- terraform-aws-flask/infra.tf
+++ terraform-aws-flask/infra.tf
@@ -13,14 +13,28 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "m4_instance" "app_server" {
+resource "aws_instance" "m4_instance" {
   count         = 5
   ami           = "ami-0149b2da6ceec4bb0"
   instance_type = "M4.large"
 }
 
-resource "t2_instance" "app_server" {
+resource "aws_instance" "t2_instance" {
   count         = 4
   ami           = "ami-0149b2da6ceec4bb0"
   instance_type = "T2.large"
+}
+
+resource "aws_elb" "load-balancer" {
+  name = "flask-load-balancer"
+  availability_zones = ["us-east-1"]
+
+  listener {
+    instance_port     = 5000 # Flask listens on port 5000
+    instance_protocol = "http"
+    lb_port           = 80 # The load balancer receives requests on the classic HTTP port 80
+    lb_protocol       = "http"
+  }
+
+  instances = [aws_instance.m4_instance[0].id, aws_instance.m4_instance[1].id, aws_instance.m4_instance[2].id, aws_instance.m4_instance[3].id, aws_instance.m4_instance[4].id, aws_instance.t2_instance[0].id,aws_instance.t2_instance[1].id,aws_instance.t2_instance[2].id,aws_instance.t2_instance[3].id]
 }
