ebc0a4d246681cbda4bc8e4d3676c85e324818e1
Christophe St-Georges <christophe.st-georges@polymtl.ca>
2022-10-05 18:59:31 -0400

    Added a route for each cluster in the webapp

--- web-app/app.py
+++ web-app/app.py
@@ -4,9 +4,19 @@ import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
+def base():
+    return f'<h1>Hi! Please go to cluster 1 or cluster 2.</h1>'
+
+@app.route('/cluster1')
+def cluster_one():
     app.config.from_prefixed_env()
     machine_id = app.config['MACHINEID']
-    return f'<h1>Hello, World! FROM: {machine_id}</h1>'
+    return f'<h1>Welcome to cluster 1! From {machine_id}</h1>'
+
+
+@app.route('/cluster2')
+def cluster_two():
+    app.config.from_prefixed_env()
+    machine_id = app.config['MACHINEID']
+    return f"<h1>Here is cluster 2! You're talking to {machine_id}</h1>"
