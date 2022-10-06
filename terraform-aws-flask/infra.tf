0915011a0a5aaba503a127523f8afae3e87c5d2d
Jimmy Bell <jimmy.bell@polymtl.ca>
2022-10-05 22:33:42 -0400

    Modified typos for terraform launch. Added teapot easter egg.

--- terraform-aws-flask/infra.tf
+++ terraform-aws-flask/infra.tf
@@ -16,32 +16,36 @@ provider "aws" {
 resource "aws_instance" "m4_instance" {
   count             = 5
   ami               = "ami-0149b2da6ceec4bb0"
-  instance_type     = "M4.large"
-  availability_zone = "us-east-1"
+  instance_type     = "m4.large"
+  associate_public_ip_address = true
   user_data = templatefile("../scripts/instance-config.sh.tftpl", {
     number = count.index
   })
+  subnet_id = aws_subnet.cluster1_subnet.id
+  vpc_security_group_ids = [aws_security_group.flask_sg.id]
 }
 
 resource "aws_instance" "t2_instance" {
   count             = 4
   ami               = "ami-0149b2da6ceec4bb0"
-  instance_type     = "T2.large"
-  availability_zone = "us-east-1"
+  instance_type     = "t2.large"
+  associate_public_ip_address = true
   user_data = templatefile("../scripts/instance-config.sh.tftpl", {
     number = count.index + 5
   })
+  subnet_id = aws_subnet.cluster2_subnet.id
+  vpc_security_group_ids = [aws_security_group.flask_sg.id]
 }
 
 resource "aws_lb_target_group" "cluster1-target" {
-  name     = "tf-example-lb-tg"
+  name     = "tg-cl1"
   port     = 5000
   protocol = "HTTP"
   vpc_id   = aws_vpc.vpc.id
 }
 
 resource "aws_lb_target_group" "cluster2-target" {
-  name     = "tf-example-lb-tg"
+  name     = "tg-cl2"
   port     = 5000
   protocol = "HTTP"
   vpc_id   = aws_vpc.vpc.id
@@ -51,12 +55,14 @@ resource "aws_lb_target_group_attachment" "attachments-cluster1-m4" {
   count            = length(aws_instance.m4_instance)
   target_group_arn = aws_lb_target_group.cluster1-target.arn
   target_id        = aws_instance.m4_instance[count.index].id
+  port = 5000
 }
 
 resource "aws_lb_target_group_attachment" "attachments-cluster2-t2" {
   count            = length(aws_instance.t2_instance)
   target_group_arn = aws_lb_target_group.cluster2-target.arn
   target_id        = aws_instance.t2_instance[count.index].id
+  port = 5000
 }
 
 resource "aws_lb" "load-balancer" {
@@ -71,12 +77,12 @@ resource "aws_lb" "load-balancer" {
 resource "aws_lb_listener" "listener_http" {
   load_balancer_arn = aws_lb.load-balancer.arn
   port              = "80"
-  protocol          = "http"
+  protocol          = "HTTP"
   default_action {
     type = "fixed-response"
     fixed_response {
       content_type = "text/plain"
-      message_body = "No destination specified or you don't know what you're doing."
+      message_body = "Nothing to show here."
       status_code  = "404"
     }
   }
@@ -85,7 +91,7 @@ resource "aws_lb_listener" "listener_http" {
 # This is a load balancer rule designed to forward /cluster1 traffic to the appropriate target group
 resource "aws_lb_listener_rule" "to-cluster-1" {
   listener_arn = aws_lb_listener.listener_http.arn
-  priority     = 100
+  priority     = 101
 
   action {
     type             = "forward"
@@ -116,3 +122,23 @@ resource "aws_lb_listener_rule" "to-cluster-2" {
   }
 }
 
+# This is a load balancer rule designed to respond to /teapot traffic with the appropriate response (easter egg)
+resource "aws_lb_listener_rule" "to-teapot" {
+  listener_arn = aws_lb_listener.listener_http.arn
+  priority     = 99
+
+  action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "This is a teapot! You're not supposed to do that :)"
+      status_code  = "418"
+    }
+  }
+
+  condition {
+    path_pattern {
+      values = ["/teapot"]
+    }
+  }
+}
