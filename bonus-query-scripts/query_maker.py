61af65c313e7003a5c38f15be06be5fa6c087e8a
jeromeCleris <jerome.cleris@polymtl.ca>
2022-10-06 15:29:59 -0400

    Completed client query_maker script with logging

--- bonus-query-scripts/query_maker.py
+++ bonus-query-scripts/query_maker.py
@@ -1,24 +1,78 @@
+import enum
 from threading import Thread
 import requests
 import re
+import logging
+import time
 
 #Retrieve the URL of the load balancer to make subsequent requests
-regex = re.compile(".+us-east-1\.elb\.amazonaws\.com\",")
-for i, line in enumerate(open('../terraform-aws-flask/terraform.tfstate')):
-    for match in re.finditer(regex, line):
-        address = match.group().__str__().strip()[13:-2]
+def get_lb_url():
+    regex = re.compile(".+us-east-1\.elb\.amazonaws\.com\",")
+    for i, line in enumerate(open('../terraform-aws-flask/terraform.tfstate')):
+        for match in re.finditer(regex, line):
+            address = match.group().__str__().strip()[13:-2]
+    return address
 
 
-def make_1000_sequentially(cluster):
-    for x in range(1000):
+def make_1000_sequentially(cluster, address):
+    for x in range(10):
         print('t1 r '+x.__str__())
         requests.get('http://'+address+cluster)
 
-def make_500_sequentially(cluster):
+def make_500_sequentially(cluster, address):
     for x in range(500):
         print('t2 r '+x.__str__())
         requests.get('http://'+address+cluster)
 
-Thread(target = make_1000_sequentially('/cluster1'),).start()
-Thread(target = make_500_sequentially('/cluster1'),).start()
+def thread_1000(cluster, address):
+    logging.info("Thread %s: starting 1000 requests", cluster)
+    make_1000_sequentially(cluster, address)
+    logging.info("Thread %s: finishing 1000 requests", cluster)
 
+def thread_500_sleep_500(cluster, address):
+    logging.info("Thread %s: starting 500 requests", cluster)
+    make_500_sequentially(cluster, address)
+    logging.info("Thread %s: sleeping 1 minute", cluster)
+    time.sleep(60)
+    logging.info("Thread %s: continuing 500 more requests", cluster)
+    make_500_sequentially(cluster, address)
+    logging.info("Thread %s: finishing 500, sleep, 500 requests", cluster)
+        
+def thread_function(name):
+    logging.info("Thread %s: starting", name)
+    time.sleep(2)
+    logging.info("Thread %s: finishing", name)
+
+def wait_for_threads(threads):
+    for index, thread in enumerate(threads):
+        thread.join()
+        logging.info("Main : thread %d done", index + 1)
+
+def set_up_cluster_threads(threadList, cluster, address):
+    logging.info("Creating %s threads",cluster)
+    t1 = Thread(target=thread_1000, args=(cluster, address,))
+    t2 = Thread(target =thread_500_sleep_500, args=(cluster, address,))
+    threadList.append(t1)
+    threadList.append(t2)
+
+    logging.info("Starting %s threads", cluster)
+    t1.start()
+    t2.start()
+
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    address = get_lb_url()
+    logging.info("Loadbalancer url: {}".format(address))
+
+    threads = list()
+    set_up_cluster_threads(threads, '/cluster1', address)
+    wait_for_threads(threads)
+    logging.info("Cluster 1 Threads done")
+    threads.clear()
+
+    set_up_cluster_threads(threads, '/cluster2', address)
+    wait_for_threads(threads)
+    logging.info("Cluster 2 Threads done")
