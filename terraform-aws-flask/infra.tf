7beab00fb5ad986c400e50ad7ac7b1e80b55404f
Jimmy Bell <jimmy.bell@polymtl.ca>
2022-10-05 20:17:44 -0400

    load balancer fixes and listener rules to paths cluster1, cluster2

--- terraform-aws-flask/infra.tf
+++ terraform-aws-flask/infra.tf
@@ -27,25 +27,22 @@ resource "aws_instance" "t2_instance" {
   availability_zone = "us-east-1"
 }
 
-resource "aws_vpc" "cluster1-vpc" {
+resource "aws_vpc" "cluster-vpc" {
   cidr_block = "10.0.0.0/16"
-}
-resource "aws_vpc" "cluster2-vpc" {
-  cidr_block = "10.1.0.0/16"
 }
 
 resource "aws_lb_target_group" "cluster1-target" {
   name     = "tf-example-lb-tg"
-  port     = 80
+  port     = 5000
   protocol = "HTTP"
-  vpc_id   = aws_vpc.cluster1-vpc.id
+  vpc_id   = aws_vpc.cluster-vpc.id
 }
 
 resource "aws_lb_target_group" "cluster2-target" {
   name     = "tf-example-lb-tg"
-  port     = 80
+  port     = 5000
   protocol = "HTTP"
-  vpc_id   = aws_vpc.cluster2-vpc.id
+  vpc_id   = aws_vpc.cluster-vpc.id
 }
 
 resource "aws_lb_target_group_attachment" "attachments-cluster1-m4" {
@@ -60,16 +57,54 @@ resource "aws_lb_target_group_attachment" "attachments-cluster2-t2" {
   target_id = aws_instance.t2_instance[count.index].id
 }
 
-resource "aws_elb" "load-balancer" {
+resource "aws_lb" "load-balancer" {
   name = "flask-load-balancer"
-  availability_zones = ["us-east-1"]
+}
 
-  listener {
-    instance_port     = 5000 # Flask listens on port 5000
-    instance_protocol = "http"
-    lb_port           = 80 # The load balancer receives requests on the classic HTTP port 80
-    lb_protocol       = "http"
+resource "aws_lb_listener" "listener_http" {
+  load_balancer_arn = aws_lb.load-balancer.arn
+  port = "80"
+  protocol = "http"
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "No destination specified or you don't know what you're doing."
+      status_code =  "404"
+    }
+  }
+}
+
+# This is a load balancer rule designed to forward /cluster1 traffic to the appropriate target group
+resource "aws_lb_listener_rule" "to-cluster-1" {
+  listener_arn = aws_lb_listener.listener_http.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.cluster1-target.arn
   }
 
-  instances = [aws_instance.m4_instance[0].id, aws_instance.m4_instance[1].id, aws_instance.m4_instance[2].id, aws_instance.m4_instance[3].id, aws_instance.m4_instance[4].id, aws_instance.t2_instance[0].id,aws_instance.t2_instance[1].id,aws_instance.t2_instance[2].id,aws_instance.t2_instance[3].id]
+  condition {
+    path_pattern {
+      values = ["/cluster1"]
+    }
+  }
+}
+
+# This is a load balancer rule designed to forward /cluster2 traffic to the appropriate target group
+resource "aws_lb_listener_rule" "to-cluster-2" {
+  listener_arn = aws_lb_listener.listener_http.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.cluster2-target.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/cluster2"]
+    }
+  }
 }
